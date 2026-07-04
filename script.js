173b3f9ea9737fa8959d544193382b71ba8f8380
dwaddumbuya <85499455+dwaddumbuya@users.noreply.github.com>
2026-07-04 17:32:36 +0000

    Implement currency conversion from Dalasis to Dollars
    Added functionality to convert Dalasis to Dollars on button click.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let monInput = document.getElementById("montantDalasis");
+let monBouton = document.getElementById ("boutonConvertir");
+let monResultat = document.getElementById("resultat");
+monBouton.addEventListener('click', function() { 
 
+let valeurTaper = monInput.value
+console.log(valeurTaper);
+let montantDollars = (valeurTaper / 70).toFixed(2);
+monResultat.textContent = `Le résultat est de ${montantDollars} dollars`
+});
